012c17390fc323ef0c1e99193785f174044bd4c8
martinvidela <videla116@gmail.com>
2023-07-15 02:32:46 -0300

    websocket

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 //--Imports
 import express from 'express'
 import { routerProducts } from './routes/productos.routes.js';
+import { engine } from 'express-handlebars';
 import { routerCarts } from './routes/carts.routes.js';
 import { __dirname } from './utils.js';
 import path from 'path'
+import { Server } from 'socket.io'
+import { viewRoutes } from './routes/views.routes.js';
 
 
 const port = 8080;
@@ -19,7 +22,13 @@ app.use(express.urlencoded({ extended: true })); // entender json de forms
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'/public')))
 
+//--Handlebars config
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, '/views'));
+
 //--Routes
+app.use('/', viewRoutes)
 app.use('/api/productos', routerProducts)
 app.use('/api/carts', routerCarts)
 
